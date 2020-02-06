656bbc88023569e097895d50b4c0b54aba083972
ykkim6@hotmail.com <ykkim6@hotmail.com>
2020-02-06 00:33:42 -0500

    Updated

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -18,15 +18,15 @@ export class ProjectsComponent implements OnInit {
   public desc: boolean = false;
   public video: boolean = false;
   public startVideo: boolean = true;
+  public userAgent = navigator.userAgent || navigator.vendor;
   // @ViewChild('option') option: ElementRef;
 
   ngOnInit() {
 
     this.projs = new Projects();
     this.project_desc = this.projs.getProject();
-    var userAgent = navigator.userAgent || navigator.vendor;
 
-    if (!(/iPad|iPhone|iPod/.test(userAgent)) || window.innerWidth >= 450) {
+    if (!(/iPad|iPhone|iPod/.test(this.userAgent)) || window.innerWidth >= 450) {
       if (this.project_desc[this.project_desc.length - 1].length == 1) {
         this.project_desc[this.project_desc.length - 1].push({ desc: [], image: "empty" });
       }
@@ -36,32 +36,43 @@ export class ProjectsComponent implements OnInit {
   //[ngClass] using this.left and this.right works but if you move a mouse too fast, it sometimes doesn't detect (mouseleave)
   //(mouseenter) and (mouseleave) with event solved the problem
   openMenu = (event) => {
-    // this.left = "animated slideInLeft";
-    // this.right = "animated slideInRight";
+
     let options = event.target.children;
-    for (let i = 0; i < options.length; i++) {
-      if (i < 3) {
-        options[i].classList.remove("animated", "slideOutLeft");
-        options[i].classList.add("animated", "slideInLeft");
-      } else {
-        options[i].classList.remove("animated", "slideOutRight");
-        options[i].classList.add("animated", "slideInRight");
+    if (!(/iPad|iPhone|iPod/.test(this.userAgent)) || window.innerWidth >= 450) {
+      for (let i = 0; i < options.length; i++) {
+        if (i < 3) {
+          options[i].classList.remove("animated", "slideOutLeft");
+          options[i].classList.add("animated", "slideInLeft");
+        } else {
+          options[i].classList.remove("animated", "slideOutRight");
+          options[i].classList.add("animated", "slideInRight");
+        }
+      }
+    } else {
+      for (let i = 0; i < options.length; i++) {
+        options[i].classList.remove("animated", "fadeOut");
+        options[i].classList.add("animated", "fadeIn");
       }
     }
   }
 
   closeMenu = (event) => {
-    // this.left = "animated slideOutLeft";
-    // this.right = "animated slideOutRight";
 
     let options = event.target.children;
-    for (let i = 0; i < options.length; i++) {
-      if (i < 3) {
-        options[i].classList.remove("animated", "slideInLeft");
-        options[i].classList.add("animated", "slideOutLeft");
-      } else {
-        options[i].classList.remove("animated", "slideInRight");
-        options[i].classList.add("animated", "slideOutRight");
+    if (!(/iPad|iPhone|iPod/.test(this.userAgent)) || window.innerWidth >= 450) {
+      for (let i = 0; i < options.length; i++) {
+        if (i < 3) {
+          options[i].classList.remove("animated", "slideInLeft");
+          options[i].classList.add("animated", "slideOutLeft");
+        } else {
+          options[i].classList.remove("animated", "slideInRight");
+          options[i].classList.add("animated", "slideOutRight");
+        }
+      }
+    } else {
+      for (let i = 0; i < options.length; i++) {
+        options[i].classList.remove("animated", "fadeIn");
+        options[i].classList.add("animated", "fadeOut");
       }
     }
   }
@@ -73,7 +84,9 @@ export class ProjectsComponent implements OnInit {
     each_project.getElementsByClassName("proj-desc")[0].style.display = "block";
     if (type == "video") {
       each_project.getElementsByClassName("video")[0].style.display = "block";
+      each_project.getElementsByClassName("video")[0].play();
       each_project.getElementsByClassName("desc-block")[0].style.display = "none";
+
     } else {
       each_project.getElementsByClassName("video")[0].style.display = "none";
       each_project.getElementsByClassName("desc-block")[0].style.display = "block";
@@ -88,12 +101,11 @@ export class ProjectsComponent implements OnInit {
   }
 
   playVideo = (event) => {
-    this.startVideo = this.startVideo ? false : true;
-
     if (this.startVideo) {
       event.target.play();
     } else {
       event.target.pause();
     }
+    this.startVideo = this.startVideo ? false : true;
   }
 }
